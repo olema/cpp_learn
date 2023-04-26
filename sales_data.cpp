59bca252703cc3bd4c939ef93d80c85be4dd20f4
Oleg Matushkin <matushkin.oleg@gmail.com>
2023-04-26 09:15:03 +0300

    sales_data modif

--- sales_data.cpp
+++ sales_data.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+#include <string>
+//#include "Sales_data.h"
+
+struct Foo {
+	int a;
+	unsigned b;
+};
+
+struct Sales_data {
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
 
 int main() {
-	struct Sales_data {
-		std::string bookNo;
-		unsigned units_sold = 0;
-		double revenue = 0.0;
-	};
 
-	Sales_data accum, trans, *salesptr;
-	salesptr = &trans;
+	Sales_data data1, data2;
+	double price = 0;
 
-	trans.bookNo = "1kjhkjh";
+	std::cin >> data1.bookNo >> data1.units_sold >> price;
+	data1.revenue = data1.units_sold * price;
+	std::cin >> data2.bookNo >> data2.units_sold >> price;
+	data2.revenue = data2.units_sold * price;
 
-	std::cout << trans.bookNo << std::endl;
-	std::cout << salesptr->bookNo << std::endl;
+	if (data1.bookNo == data2.bookNo) {
+		unsigned totalCnt = data1.units_sold + data2.units_sold;
+		double totalRevenue = data1.revenue + data2.revenue;
+		std::cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << " ";
+		if (totalCnt != 0)
+			std::cout << totalRevenue/totalCnt << std::endl;
+		else
+			std::cout << "(no sales)" << std::endl;
+		return 0;
+	} else {
+		std::cerr << "Data must refer to the same ISBN" << std::endl;
+		return -1;
+	}
 }
