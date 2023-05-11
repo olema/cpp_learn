65a96f4c3ec1ed598c461dd9118aa03d4360139a
Oleg Matushkin <matushkin.oleg@gmail.com>
2023-05-11 15:25:15 +0300

    testing string

---
+++ string_iterator.cpp
@@ -0,0 +1,11 @@
+#include "using_define.h"
+
+int main() {
+    string s;
+    char c;
+
+    cout << "input string: " << endl;
+    getline(cin,s);
+
+    if (!
+}
---
+++ using_define.h
@@ -0,0 +1,10 @@
+#ifndef USING_DEFINE
+#define USING_DEFINE
+#include <string>
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+#endif
