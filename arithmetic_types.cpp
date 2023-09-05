4497ac2f483284c3ebdaf71056c7cfbee8809c6b
Oleg Matushkin <matushkin.oleg@gmail.com>
2023-09-05 07:55:27 +0300

    arithmetic types

---
+++ arithmetic_types.cpp
@@ -0,0 +1,16 @@
+#include "using_define.h"
+
+int main() {
+    bool b = true;
+    char c = 'a'; // symbol 8 bits
+    wchar_t wc = 'w'; // wide symbol 16 bits
+    char16_t c16t = 'Б'; // unicode 16 bits
+    char32_t c32t = 'Г'; // unicode 32 bits
+    short s = 1; // short int 16 bits
+    int i = 16; // int 16 bits
+    long l = 32; // long int 32 bits
+    long long ll = 64; // long long int 64 bits
+    float f = 3.14; // 6 numbers
+    double d = 2.71828; // 10 numbers
+    long double ld = 1.41421356237; // 10 numbers
+}
---
+++ std_cin_from_file.cpp
@@ -0,0 +1,12 @@
+#include "using_define.h"
+
+int main() {
+
+    int i = 0, sum = 0;
+    
+    while (cin >> i) {
+        sum += i;
+    }
+    cout << sum << endl;
+    return 0;
+}
